b4ff4ed3fc7f003b3ee2f87b3734cb09c056a6df
hamdysaad20 <hamdysaad.biz@gmail.com>
2022-02-23 08:05:56 +0200

    a

--- components/messageComponents/RHmessage.js
+++ components/messageComponents/RHmessage.js
@@ -16,7 +16,7 @@ function RHmessage() {
 
       <div className='max-w-[30%] ml-4 mt-9 inline-block lg:mt-4'>
         <div className='bg-red-700/90  font-normal  mx-3 px-4 py-1 rounded-lg'>
-          <h1 className='text-neutral-200 text-xl'> Hi i'am admin here!</h1>
+          <h1 className='text-neutral-200 text-xl'> Hi iam admin here!</h1>
         </div>
         <div className='bg-red-900/60 h-2 w-2 absolute mt-[-7px] rounded-full mr-[-2px] -rotate-12'></div>
       </div>
